b7137bb4bf60bc2c9d8d21e9cc4d7fcbcd2f0aa4
Arda Cinar <kuzux92@gmail.com>
2013-10-27 09:41:25 +0200

    List subreddit entries

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import requests
 import json
 import os
@@ -13,6 +15,20 @@ def login(username, password):
     if res['json']['errors'] != []:
         raise Exception(res['json']['errors'][0])
 
-    return res['json']['data']
+    client.modhash = res['json']['data']['modhash']
+    client.user = username
+    client.headers['X-Modhash']=client.modhash
 
-print login('GifExploderBot', os.environ['GIFEXPLODERBOT_PASSWORD'])
+    return client
+
+def new_stories(client, subreddit):
+    params = {'limit': 100,}
+    url = "http://www.reddit.com/r/{sr}/new.json".format(sr=subreddit)
+    req = client.get(url, params=params)
+    res = json.loads(req.text)
+    for story in res['data']['children']:
+        print "{n}: {u}".format(n=story['data']['title'].encode('utf-8'), u=story['data']['url'])
+    return res
+
+client = login('GifExploderBot', os.environ['GIFEXPLODERBOT_PASSWORD'])
+new_stories(client, 'MapPorn')
